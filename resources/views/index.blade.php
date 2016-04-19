3481a99a1519d63279192c665c4766c193477634
kgh458 <kgh458@mail.missouri.edu>
2016-04-19 12:22:02 -0500

    template
    added with routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,53 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends( 'template' );
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">So heres the plan:</div>
-                <h2>-Kara and Will need to connect her bootstrap to this app</h2>
-                <h2>-Will then need to route and link everything correctly page to page (only get requests this far)</h2>
-                <h2>-Dustin then needs to make the tables</h2>
-                <h2>-Everyone then needs to add the information into the tables</h2>
-                <h2>-Kara and Maria need to make the application how we want on the front end</h2>
-                <h2>-Will then needs to link the back end to the front; during this time Dustin needs to be checking our DB design and changing it as necessary, and give correct access to the application based on a type of user</h2>
-                <h2>-Alright, break!</h2>
-                <div class="title">Laravel 5</div>
-            </div>
+@yields( 'content' );
+    <div class="container">
+        <div class="content">
+            <div class="title">So heres the plan:</div>
+            <h2>-Kara and Will need to connect her bootstrap to this app</h2>
+            <h2>-Will then need to route and link everything correctly page to page (only get requests this far)</h2>
+            <h2>-Dustin then needs to make the tables</h2>
+            <h2>-Everyone then needs to add the information into the tables</h2>
+            <h2>-Kara and Maria need to make the application how we want on the front end</h2>
+            <h2>-Will then needs to link the back end to the front; during this time Dustin needs to be checking our DB design and changing it as necessary, and give correct access to the application based on a type of user</h2>
+            <h2>-Alright, break!</h2>
+            <div class="title">Laravel 5</div>
         </div>
-    </body>
-</html>
+    </div>

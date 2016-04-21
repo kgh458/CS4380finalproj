1cf0f20103263c4a21f951e17de2e36e9ef3e7f7
drrkvf <dustinrios@mwc-041099.dhcp.missouri.edu>
2016-04-21 15:45:25 -0500

    create tables made starting to fill fields

---
+++ database/migrations/2016_04_21_194307_create_table_address.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAddress extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('address');
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('addressId');
+            $table->string('str_addr', 30);
+            $table->string('zip_code', 6);
+            $table->string('state', 2);
+            $table->string('city', 15);
+            $table->primary('addressId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('address');
+    }
+}
